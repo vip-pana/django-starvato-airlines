e26739584247f8a866b061b1970a7312b17e6139
pana <vip_P4na@protonmail.com>
2022-04-16 18:25:08 +0200

    email

--- airlines5/airlines5/settings.py
+++ airlines5/airlines5/settings.py
@@ -146,6 +146,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'fakestarvatoairlines@gmail.com'
-EMAIL_HOST_PASSWORD = '' #metti un fittizio prima di continuare
+EMAIL_HOST_PASSWORD = 'oWi32FCVxbsNtD' #metti un fittizio prima di continuare
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False    
